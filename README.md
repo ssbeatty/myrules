58a668475d12e76d3b9c0cfc6c7709f957e83b36
sasaba <wang918562230@qq.com>
2025-05-26 03:48:16 +0000

    init project

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # myrules
+
+参考: https://github.com/Loyalsoldier/clash-rules
+
+自用
---
+++ configs/merge.js
@@ -0,0 +1,30 @@
+// 规则
+const newRules = [
+	  "RULE-SET,mydirect,DIRECT",
+      "RULE-SET,applications,DIRECT",
+	  "RULE-SET,private,DIRECT",
+	  "RULE-SET,reject,REJECT",
+	  "RULE-SET,icloud,DIRECT",
+	  "RULE-SET,apple,DIRECT",
+	  "RULE-SET,google,Proxies",
+	  "RULE-SET,proxy,Proxies",
+	  "RULE-SET,direct,DIRECT",
+	  "RULE-SET,lancidr,DIRECT",
+	  "RULE-SET,cncidr,DIRECT",
+	  "RULE-SET,telegramcidr,Proxies",
+		"GEOIP,LAN,DIRECT",
+  ];
+
+
+ // 程序入口
+function main(config) {
+    let oldRules = config.rules;
+
+  // 将新的规则添加到现有的规则中
+  config.rules = newRules.concat(oldRules);
+
+	config["dns"]["nameserver"].unshift("system");
+	
+  // 返回修改后的配置
+  return config;
+}
