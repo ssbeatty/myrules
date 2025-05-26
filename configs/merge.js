51191f0aa96bb9c12dfc78c724bf9cdb0f670248
sasaba <wang918562230@qq.com>
2025-05-26 20:24:06 +0800

    add rule

--- configs/merge.js
+++ configs/merge.js
@@ -1,18 +1,19 @@
 // 规则
 const newRules = [
-	  "RULE-SET,mydirect,DIRECT",
+      "RULE-SET,mydirect,DIRECT",
+      "RULE-SET,myblack,Proxies",
       "RULE-SET,applications,DIRECT",
-	  "RULE-SET,private,DIRECT",
-	  "RULE-SET,reject,REJECT",
-	  "RULE-SET,icloud,DIRECT",
-	  "RULE-SET,apple,DIRECT",
-	  "RULE-SET,google,Proxies",
-	  "RULE-SET,proxy,Proxies",
-	  "RULE-SET,direct,DIRECT",
-	  "RULE-SET,lancidr,DIRECT",
-	  "RULE-SET,cncidr,DIRECT",
-	  "RULE-SET,telegramcidr,Proxies",
-		"GEOIP,LAN,DIRECT",
+      "RULE-SET,private,DIRECT",
+      "RULE-SET,reject,REJECT",
+      "RULE-SET,icloud,DIRECT",
+      "RULE-SET,apple,DIRECT",
+      "RULE-SET,google,Proxies",
+      "RULE-SET,proxy,Proxies",
+      "RULE-SET,direct,DIRECT",
+      "RULE-SET,lancidr,DIRECT",
+      "RULE-SET,cncidr,DIRECT",
+      "RULE-SET,telegramcidr,Proxies",
+      "GEOIP,LAN,DIRECT",
   ];
 
 
@@ -20,11 +21,11 @@ const newRules = [
 function main(config) {
     let oldRules = config.rules;
 
-  // 将新的规则添加到现有的规则中
-  config.rules = newRules.concat(oldRules);
+    // 将新的规则添加到现有的规则中
+    config.rules = newRules.concat(oldRules);
 
-	config["dns"]["nameserver"].unshift("system");
+    config["dns"]["nameserver"].unshift("system");
 	
-  // 返回修改后的配置
-  return config;
+    // 返回修改后的配置
+    return config;
 }
