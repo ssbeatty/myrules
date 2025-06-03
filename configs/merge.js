926121449f2f10e27b69afcd60c1b0d892e240d5
guoxi.wang <guoxi.wang@whes.com>
2025-06-03 09:44:02 +0800

    add new domain to black and direct lists; reorganize rule priorities in merge.js

--- configs/merge.js
+++ configs/merge.js
@@ -1,31 +1,38 @@
-// 规则
-const newRules = [
-      "RULE-SET,mydirect,DIRECT",
-      "RULE-SET,myblack,Proxies",
-      "RULE-SET,applications,DIRECT",
-      "RULE-SET,private,DIRECT",
-      "RULE-SET,reject,REJECT",
-      "RULE-SET,icloud,DIRECT",
-      "RULE-SET,apple,DIRECT",
-      "RULE-SET,google,Proxies",
-      "RULE-SET,proxy,Proxies",
-      "RULE-SET,direct,DIRECT",
-      "RULE-SET,lancidr,DIRECT",
-      "RULE-SET,cncidr,DIRECT",
-      "RULE-SET,telegramcidr,Proxies",
-      "GEOIP,LAN,DIRECT",
-  ];
+// 高优先级
+const newFirstRules = [
+    "RULE-SET,myblack,Proxies",
+    "RULE-SET,mydirect,DIRECT",
+    "RULE-SET,applications,DIRECT",
+];
 
 
- // 程序入口
+// 低优先级
+const newLastRules = [
+    "RULE-SET,private,DIRECT",
+    "RULE-SET,reject,REJECT",
+    "RULE-SET,icloud,DIRECT",
+    "RULE-SET,apple,DIRECT",
+    "RULE-SET,google,Proxies",
+    "RULE-SET,proxy,Proxies",
+    "RULE-SET,direct,DIRECT",
+    "RULE-SET,lancidr,DIRECT",
+    "RULE-SET,cncidr,DIRECT",
+    "RULE-SET,telegramcidr,Proxies",
+    "GEOIP,LAN,DIRECT",
+];
+
+
+
+
+// 程序入口
 function main(config) {
     let oldRules = config.rules;
 
-    // 将新的规则添加到现有的规则中
-    config.rules = newRules.concat(oldRules);
+    let temp = oldRules.concat(newLastRules);
+    config.rules = newFirstRules.concat(temp);
 
     config["dns"]["nameserver"].unshift("system");
-	
+
     // 返回修改后的配置
     return config;
 }
