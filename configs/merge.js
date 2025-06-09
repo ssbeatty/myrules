3afe271e8f85a80c26d1fba8518df94b04c19ba6
guoxi.wang <guoxi.wang@whes.com>
2025-06-09 14:32:28 +0800

    update configs

--- configs/merge.js
+++ configs/merge.js
@@ -1,8 +1,9 @@
 // 高优先级
 const newFirstRules = [
-    "RULE-SET,myblack,Proxies",
-    "RULE-SET,mydirect,DIRECT",
-    "RULE-SET,myai,AI",
+    "RULE-SET,myblack-domain,Proxies",
+    "RULE-SET,mydirect-domain,DIRECT",
+    "RULE-SET,myai-domain,AI",
+    "RULE-SET,myblack-classical,Proxies",
     "RULE-SET,applications,DIRECT",
 ];
 
